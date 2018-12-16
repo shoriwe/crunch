f43b7772db58def7661439d85456580d6fae782e
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2018-12-15 21:48:12 -0500

    Subo la script de crunch
    Solo subo por primera vez la script

---
+++ crunch/__init__.py
@@ -0,0 +1,1 @@
+from .crunch import Crunch
---
+++ crunch/crunch.py
@@ -0,0 +1,45 @@
+from string import ascii_letters, digits, punctuation, whitespace
+
+
+class Crunch:
+    def __init__(self, letras=ascii_letters + digits + punctuation + whitespace, remover=''):
+        self.letras = letras
+        for caracter in remover:
+            self.letras.replace(caracter, '')
+        self.tamano = len(self.letras) - 1
+
+    def generar(self, min_l, max_l=None, funcion=print,out_list=True):
+        if max_l == None:
+            max_l = min_l
+        if out_list:
+            salida = []
+        for numero in range(min_l, max_l + 1):
+            puestos = [0 for x in range(numero)]
+            for n in range((self.tamano + 1) ** numero):
+                palabra, puestos = self._crear_palabra(puestos)
+                if out_list:
+                    salida.append(palabra)
+                if funcion:
+                    funcion(palabra)
+        if out_list:
+            return salida
+
+    def _actualizar_puestos(self, puestos):
+        aumentar = True
+        for n in range(len(puestos)):
+            if puestos[n] <= self.tamano:
+                if aumentar:
+                    puestos[n] += 1
+                    if puestos[n] > self.tamano:
+                        puestos[n] = 0
+                        aumentar = True
+                    else:
+                        aumentar = False
+            else:
+                puestos[n] = 0
+        return puestos
+
+    def _crear_palabra(self, puestos):
+        salida = ''.join(self.letras[n] for n in puestos)
+        puestos = self._actualizar_puestos(puestos)
+        return salida, puestos
